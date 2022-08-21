37cdf450c73415c9953f0c534db6aea5239acedb
kolbykimball <kolby.kimball10@gmail.com>
2022-08-21 14:53:21 -0600

    cards created and design on cards was started

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+var city = "";
+var searchCity = "";
+var searchBtn = "";
+var clearBtn = "";
+var currentCity = "";
+var currentTemp = "";
+var currentHumidity = "";
+var currentWind = "";
+var currentUV = "";
+
+
+//set up api key
+var APIKey = "dc1a549f2980acea09337f333e83fee1"
+//function for displaying current weather
